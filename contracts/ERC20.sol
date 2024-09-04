0355b9832e3a75094df2f790e90941177d1d6eb8
Mordecai <chingolo265@gmail.com>
2024-09-04 10:00:02 +0200

    ERC20 contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,2 +1,13 @@
 //SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import "@openzepplin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor() ERC20("@divdynamics", "DVX"){
+        _mint(msg.sender,50000**18)
+    }
+}
+
+
